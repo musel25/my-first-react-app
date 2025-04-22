ee5504ad27608c233f1e7c0dbebc3475ac8c9884
musel25 <muselemmanuel@hotmail.com>
2025-04-22 09:44:01 +0200

    class-component

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,41 @@
 import React, { useState } from "react";
 import "./App.css";
 
-const COLORS = ["pink", "green", "blue", "yellow", "purple"];
+const FunctionalInput = ({ name }) => {
+  const [todos, setTodos] = useState(["Just some demo tasks", "As an example"]);
+  const [inputVal, setInputVal] = useState("");
 
-function App() {
-  const [backgroundColor, setBackgroundColor] = useState(COLORS[1]);
+  const handleInputChange = (e) => {
+    setInputVal(e.target.value);
+  };
 
-  const onButtonClick = (color) => () => {
-    setBackgroundColor(color);
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setTodos((todo) => [...todo, inputVal]);
+    setInputVal("");
   };
 
   return (
-    <div
-      className="App"
-      style={{
-        backgroundColor,
-      }}
-    >
-      {COLORS.map((color) => (
-        <button
-          type="button"
-          key={color}
-          onClick={onButtonClick(color)}
-          className={backgroundColor === color ? "selected" : ""}
-        >
-          {color}
-        </button>
-      ))}
-    </div>
+    <section>
+      <h3>{name}</h3>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="task-entry">Enter a task: </label>
+        <input
+          type="text"
+          name="task-entry"
+          value={inputVal}
+          onChange={handleInputChange}
+        />
+        <button type="submit">Submit</button>
+      </form>
+      <h4>All the tasks!</h4>
+      <ul>
+        {todos.map((todo) => (
+          <li key={todo}>{todo}</li>
+        ))}
+      </ul>
+    </section>
   );
-}
+};
 
-export default App;
+export default FunctionalInput;
